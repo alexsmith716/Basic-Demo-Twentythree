71d1b3476e14f0b35b2e3702f79a0804dda06495
AlexSmith716 <1691290+alexsmith716@users.noreply.github.com>
2020-09-03 21:50:17 -0400

    refactor: Converted component Loading keyframes dot animation to styled-components.

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,12 +1,41 @@
 import React from 'react';
-const styles = require("./scss/Loading.scss");
+import styled, { keyframes } from 'styled-components';
 
 interface LoadingProps {
-  text: string;
+	text: string;
 }
+
+const dot = keyframes`
+	0% {
+		opacity: 0.25;
+	}
+	50% {
+		opacity: 0.25;
+	}
+	100% {
+		opacity: 1;
+	}
+`
+const DotOne = styled.span`
+	opacity: 10;
+	animation: ${dot} 1.3s infinite;
+	animation-delay: 0s;
+`;
+
+const DotTwo = styled.span`
+	opacity: 10;
+	animation: ${dot} 1.3s infinite;
+	animation-delay: 0.2s;
+`;
+
+const DotThree = styled.span`
+	opacity: 10;
+	animation: ${dot} 1.3s infinite;
+	animation-delay: 0.3s;
+`;
 
 export const Loading = ({ text }: LoadingProps) => {
 	return (
-		<div>{text}<span className={styles.one}>.</span><span className={styles.two}>.</span><span className={styles.three}>.</span></div>
+		<div>{text}<DotOne>.</DotOne><DotTwo>.</DotTwo><DotThree>.</DotThree></div>
 	);
 };
