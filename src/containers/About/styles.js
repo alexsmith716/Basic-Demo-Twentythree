9ec441831f7c88f33b6ba48b58d8aff5d8152e3e
AlexSmith716 <1691290+alexsmith716@users.noreply.github.com>
2020-09-09 23:15:52 -0400

    refactor: Broke out InfoBar and About styled-components to respective module file styles.

---
+++ src/containers/About/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import { Colors } from '../../styled/Colors';
+
+export const AboutStyled = styled.div`
+  // background-color: ${p => p.theme.colors.ivory};
+`;
+
+export const SubContainer = styled.div`
+  background-color: ${Colors.lightskyblue};
+`;
